f518bda4bcfb79d20ac0dae599dd4dd84790f823
yahya.white <your_email@example.com>
2025-12-22 11:02:35 +0300

    Initial project structure (HTML, CSS, JS files)

---
+++ move.js
@@ -0,0 +1,12 @@
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
